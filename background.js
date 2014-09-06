4dae33647f153fad9e5cc19674e3445ec529d2df
MaxWofford <max.speed.wofford@gmail.com>
2014-09-05 23:30:47 -0700

    Removed old disasters avoided references

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 // Set up context menu at install time.
 chrome.runtime.onInstalled.addListener(function() {
-  var context = "selection";
-  var title = "Schedule download...";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                         "id": "context" + context});  
+	var context = "selection";
+	var title = "Schedule download...";
+	var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": "context" + context});	
 });
 
 // add click event
@@ -11,7 +10,12 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // The onClicked callback function.
 function onClickHandler(info, tab) {
-  var sText = info.selectionText;
-  var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);  
-  window.open(url, '_blank');
-};
+
+}
+
+function download(link) {
+	chrome.downloads.download({
+		url: link,
+		filename: "suggested/filename/with/relative.path" // Optional
+	});
+}
